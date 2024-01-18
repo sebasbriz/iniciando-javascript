64d205046181da628530fcf6752a9358023849fc
sebasbriz <sebasbrizuela2003@gmail.com>
2024-01-17 23:48:35 -0300

    se agrego el ejercicio de operadores aritmeticos

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,52 @@
+//suma
+let str1 = "hola ";
+let str2 = "como estas?";
+let resultado = str1 + str2;
+console.log(`El resultado de la suma es ${resultado}`);
+
+//resta
+let nr1 = 56;
+let nr2 = 7;
+let str3 = "Hola";
+let nr3 = 123;
+let result1 = nr1 - nr2;
+let result2 = str3 - nr3;
+console.log(`El resultado 1 es ${result1} y el resultado 2 es ${result2}`);
+
+//la multiplicación
+let mnr1 = 25;
+let mnr2 = 136;
+let mstr1 = "Hola";
+let mnr3 = 3;
+let mresult1 = mnr1 * mnr2;
+let mresult2 = mstr1 * mnr3;
+console.log(mresult1, mresult2);
+
+//división
+let dnr1 = 10;
+let dnr2 = 2;
+let dresult1 = dnr1 / dnr2;
+console.log(dresult1);
+
+//exponenciacion
+let enr1 = 2;
+let enr2 = 3;
+let eresult1 = enr1 ** enr2;
+console.log(eresult1);
+
+//modulo
+debugger;
+let monr1 = 23;
+let monr2 = 5;
+let moresult1 = monr1 % monr2;
+console.log(`${monr1} % ${monr2} = ${moresult1}`);
+
+let monr3 = 8;
+let monr4 = 2;
+let moresult2 = monr3 % monr4;
+console.log(`${monr3} % ${monr4} = ${moresult2}`);
+
+let monr5 = 15;
+let monr6 = 4;
+let moresult3 = monr5 % monr6;
+console.log(`${monr5} % ${monr6} = ${moresult3}`);
