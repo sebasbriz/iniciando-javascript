3861c2e996b6c53fc9d70857a15c73389eb50805
sebasbriz <sebasbrizuela2003@gmail.com>
2024-01-18 22:55:48 -0300

    se agrego la tarea 2.4

---
+++ tarea2.4.js
@@ -0,0 +1,15 @@
+/*
+Crea variables para tres numeros: a, b, c. Actualiza
+los valores de esas variables usando los operadores de asignacion:
+-suma b en a
+-divide a con c
+-reemplaza el valor de c con el modulo de c y b
+-imprimir en consola los tres numeros
+*/
+let a = 4, b = 5, c = 6;
+a += b;
+console.log(a);
+a /= c;
+console.log(a);
+c %= b;
+console.log(c);
