a879d863f81b4eeca5f28e198564b32f334dca39
sebasbriz <sebasbrizuela2003@gmail.com>
2024-01-18 22:36:23 -0300

    se agrego el proyecto 2

---
+++ proyecto_2.js
@@ -0,0 +1,11 @@
+//ejercicio 1
+let miles = 200;
+let km = (miles*1.609);
+console.log(`La distancia de ${km} km es ${miles} millas`);
+
+//ejercicio 2
+//imc=peso(kg)/altura(m)
+let kg = prompt("Ingrese su peso (kg)");
+let alt = prompt("Ingrese su altura (m2)");
+let imc = (kg/alt**2);
+console.log(imc);
