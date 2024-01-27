6697695c354d12092820b58d73f8eb0c92e2a901
sebasbriz <sebasbrizuela2003@gmail.com>
2024-01-27 12:32:58 -0300

    se añade el ejercicio 3.1 de arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+/*Ejercicio 3.1
+-Crea un array para usar en tus compras al supermercado, una lista de tres items.
+-Revisa tu lista usando length.
+-Actualiza uno de los elementos.
+-Muestra la lista entera en consola
+*/
+let listSup = ["Cafe", "Azucar", "Pan"];
+console.log(`Cantidad de items en la lista del super: ${listSup.length}`);
+listSup[1] = "Leche";
+console.log(`Lista del super actualizada: ${listSup}`);
